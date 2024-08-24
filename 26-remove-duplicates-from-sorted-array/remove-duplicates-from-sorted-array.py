4dbbf034dcf429a2fc620998f1310c8e269c6b0a
Vamshipapisetty1 <161233094+Vamshipapisetty1@users.noreply.github.com>
2024-08-23 23:23:15 -0400

    Time: 68 ms (89.97%) | Memory: 17.9 MB (43.98%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,37 +1,12 @@
-# class Solution(object):
-#     def removeDuplicates(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: int
-#         """
-#         if not nums:
-#             return 0
-        
-#         # Initialize the slow pointer
-#         slow = 0
-        
-#         # Iterate through the array with the fast pointer
-#         for fast in range(1, len(nums)):
-#             if nums[fast] != nums[slow]:
-#                 slow += 1
-#                 nums[slow] = nums[fast]
-        
-#         # Return the length of the unique elements
-#         return slow + 1
-class Solution(object):
-    def removeDuplicates(self, nums):
+from typing import List
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
         if not nums:
             return 0
-    
-        # Initialize the first pointer
         i = 0
-    
-        # Start iterating from the second element
         for j in range(1, len(nums)):
-            # If the current element is different from the last unique element
-            if nums[j] != nums[i]:
+            if nums[j] != nums[i]:  
                 i += 1
                 nums[i] = nums[j]
-    
-        # The number of unique elements is i + 1
         return i + 1
+        
